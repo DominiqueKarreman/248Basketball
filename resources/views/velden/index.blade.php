a3aaa6687aa6f7a25312ed5412eeef0d8b863f17
Dominique Karreman <s1169919@student.windesheim.nl>
2023-03-02 10:40:20 +0100

    updated competitie option on index velden

--- resources/views/velden/index.blade.php
+++ resources/views/velden/index.blade.php
@@ -67,7 +67,7 @@
                         <th scope="row"
                             class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
 
-                            {{ $veld->competitie }}
+                            {{ $veld->competitie ? 'Ja' : 'Nee' }}
 
                         </th>
                         <th scope="row"

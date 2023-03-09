6f6fadcedfd6263f474edc8eb32319aef1de08b5
Dominique Karreman <90785931+DominiqueKarreman@users.noreply.github.com>
2023-03-09 14:56:20 +0100

    added geboorte_datum to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'geboorte_datum'
     ];
     protected $guard_name = 'web';
 

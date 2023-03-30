4ed6c6d93dfdbf8b8b28f8cb00a53697e7c2c473
DDuijker <djoekeduijker@live.nl>
2023-03-30 14:10:24 +0200

    removed password

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
     <x-slot name="logo">
-        {{-- <x-application-mark /> --}}
+        <x-application-mark />
     </x-slot>
 
     <x-validation-errors class="mb-4" />
@@ -38,19 +38,6 @@
                         <p class="text-red-500 mt-1">{{ $message }}</p>
                     @enderror
                 </div>
-                {{-- Make changing the passwords optional --}}
-                {{-- <div class="mt-4">
-                    <x-label for="password" value="{{ __('Password') }}" />
-                    <x-input id="password" class="block mt-1 w-full" type="password" name="password"
-                        autocomplete="new-password" />
-                </div>
-
-                <div class="mt-4">
-                    <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                    <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                        name="password_confirmation" autocomplete="new-password" />
-                </div> --}}
-
 
                 <div class="flex items-center justify-end mt-4">
 

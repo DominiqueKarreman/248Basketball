060f94ee8b1c6bb5a95c63130e5b9a6c8bbc4c5d
Dominique Karreman <s1169919@student.windesheim.nl>
2023-04-06 18:58:05 +0200

    get chats with user

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\Api\ApiUserFriendController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
 use App\Http\Controllers\Api\ApiVeldController;
+
 use App\Http\Controllers\Api\ApiEventController;
+use App\Http\Controllers\Api\ApiUserFriendController;
+use App\Http\Controllers\Api\ApiChatMessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,4 +47,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post("/friends/request/{id}/decide", [ApiUserFriendController::class, "decide"]);
     Route::delete("/friends/{id}/unfriend", [ApiUserFriendController::class, "unfriend"]);
     Route::get("/friends/provider:{provider}", [ApiUserFriendController::class, "socialFriends"]);
+
+    Route::get("/messages", [ApiChatMessageController::class, "index"]);
+    Route::get("/messages/{withUser}", [ApiChatMessageController::class, "withUser"]);
 });

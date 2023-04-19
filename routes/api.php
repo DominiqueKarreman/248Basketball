b12ff1c04d3d92866314905e7b0034b48c0a80b0
MarjonPhu <marjonphu15@gmail.com>
2023-04-19 12:26:07 +0200

    another conflicft resolve

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
 use App\Http\Controllers\Api\ApiVeldController;
+
 use App\Http\Controllers\Api\ApiEventController;
+use App\Http\Controllers\Api\ApiUserFriendController;
+use App\Http\Controllers\Api\ApiChatMessageController;
+use App\Http\Controllers\Api\ApiUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,9 +38,9 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::delete('/events/{id}', [ApiEventController::class, 'destroy']);
     //users endpoints
     Route::get('/user', [UserController::class, 'showApi']);
+    Route::get('/gebruikers', [ApiUserController::class, 'gebruikers']);
     //velden endpoints 
     Route::get('/velden', [ApiVeldController::class, 'index']);
-    Route::get('/velden/{id}', [ApiVeldController::class, 'show']);
     Route::get('/velden/sort/{lat}/{long}', [ApiVeldController::class, 'locationSorted']);
 
     Route::get("/friends", [ApiUserFriendController::class, "index"]);
@@ -46,6 +49,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post("/friends/request/{id}/decide", [ApiUserFriendController::class, "decide"]);
     Route::delete("/friends/{id}/unfriend", [ApiUserFriendController::class, "unfriend"]);
     Route::get("/friends/provider:{provider}", [ApiUserFriendController::class, "socialFriends"]);
+
     Route::get("/messages", [ApiChatMessageController::class, "index"]);
     Route::get("/messages/{withUser}", [ApiChatMessageController::class, "withUser"]);
 });

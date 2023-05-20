407f974fc7297803e4384b9c1e6527cc4fa2207c
DDuijker <djoekeduijker@live.nl>
2023-05-20 16:31:16 +0200

    Added newspolicies

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,11 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\NewsArticle;
+use Illuminate\Support\Facades\Storage;
 
 class NewsController extends Controller
 {
     public function index()
     {
         return view('news');
+    }
+
+    public function store()
+    {
+
+        //authorize the request
+        $this->authorize('create', NewsArticle::class);
+
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'is_cover' => 'boolean',
+            'short_description' => 'required',
+            'content' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+    
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('public/images');
+            $validatedData['image'] = $imagePath;
+        }
+    
+        // Create the news article
+        NewsArticle::create($validatedData);
+    
     }
 }

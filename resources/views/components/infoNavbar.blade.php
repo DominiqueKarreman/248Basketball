83ac27b0a719a94c9f927e3212fa617e6004fd4f
Dominique Karreman <s1169919@student.windesheim.nl>
2023-05-26 02:01:05 +0200

    Update infoNavbar.blade.php

--- resources/views/components/infoNavbar.blade.php
+++ resources/views/components/infoNavbar.blade.php
@@ -1,13 +1,15 @@
 @props(['class' => ''])
 
-<nav class=" border-gray-200 dark:bg-gray-900 {{ $class }}">
+<nav class="  border-gray-200 dark:bg-gray-900 {{ $class }}">
     <div class="flex w-full bg-transparent flex-wrap items-center justify-between sm:w-[90vw] mx-auto p-4">
         <a href="{{ url('/') }}" class="flex items-center">
             <img src="{{ asset('images/logo.png') }}" id="logo" alt="">
         </a>
         <div class="flex md:order-2">
-            <button type="button"
-                class="text-white bg-[#EDB12C] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('Login') }}</button>
+            <a href="{{route("login")}}">
+                <button type="button"
+                    class="text-white bg-[#EDB12C] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('Login') }}</button>
+            </a>
             <button data-collapse-toggle="navbar-cta" type="button"
                 class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-cta" aria-expanded="false">

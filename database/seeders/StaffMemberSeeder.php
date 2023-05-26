b884034a383a06337d746d5e10c068fd597bffee
Dominique Karreman <s1169919@student.windesheim.nl>
2023-05-26 02:28:51 +0200

    Update StaffMemberSeeder.php

--- database/seeders/StaffMemberSeeder.php
+++ database/seeders/StaffMemberSeeder.php
@@ -19,7 +19,7 @@ class StaffMemberSeeder extends Seeder
             "name" => "Jorg Janssens",
             "role" => "Co-founder / Skills trainer",
             "image" => "storage/staff/jorg.png",
-            "video" => "storage/staff/jorg.MP4",
+            "video" => "storage/staff/jorg.mp4",
             "description" => 'Al vanaf mijn 7e ben ik te vinden op het basketbalveld. Vanaf het eerste moment ben ik verliefd op de sport. Het basketbalveld is de plek waar ik ben opgegroeid, vele lessen heb geleerd en waar ik mij altijd thuis voel. Ik probeer altijd het maximale uit mijzelf en anderen te halen. Na vele jaren op hoog niveau meegedraaid te hebben, wil ik nu mijn aandacht besteden aan anderen. Ik houd ervan om anderen te helpen met het ontwikkelen van hun game.
         “Basketball is not just a sport, Its a lifestyle.”',
             "email" => "jorgjanssens@outlook.com",

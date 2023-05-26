38f23daab6b62a8796471b08f5289fcb8eeadae4
Dominique Karreman <s1169919@student.windesheim.nl>
2023-05-26 15:04:02 +0200

    Update infoNavbar.blade.php

--- resources/views/components/infoNavbar.blade.php
+++ resources/views/components/infoNavbar.blade.php
@@ -6,7 +6,7 @@
             <img src="{{ asset('images/logo.png') }}" id="logo" alt="">
         </a>
         <div class="flex md:order-2">
-            <a href="{{route("login")}}">
+            <a href="{{route('login')}}">
                 <button type="button"
                     class="text-white bg-[#EDB12C] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ Auth()->user() ? __('Dashboard') : __('Login') }}</button>
             </a>

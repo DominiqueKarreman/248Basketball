b5f853a41900f8064e327ba79fc552c2e3c511c2
Dominique Karreman <s1169919@student.windesheim.nl>
2023-06-06 12:22:12 +0200

    reverted changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <title>248Basketball</title>
 </head>
 
-<body class="bg-white flex flex-col h-full w-full">
+<body class="bg-black flex flex-col h-full w-full">
     <x-infoNavbar class="my-custom-class" />
     <div class="flex flex-col relative h-[1000vh]">
         <div id="gradient-overlay"></div>
@@ -197,7 +197,7 @@
     #programmeDiv {
         opacity: 0;
         margin-top: 15vh;
-        width: 90vw;
+        width: 100vw;
         transform: translateX(-100%);
         transition: opacity 1s ease-out, transform 1s ease-out;
     }
@@ -210,7 +210,7 @@
         align-content: center;
         opacity: 0;
         height: 90vh;
-        width: 90vw;
+        width: 100vw;
         animation: fade-in 1s ease-out forwards;
         animation-delay: 0.7s;
         position: relative;
@@ -221,7 +221,7 @@
         position: absolute;
         top: -9vh;
         left: 0;
-        width: 90vw;
+        width: 100vw;
         height: 100vh;
         background: linear-gradient(to bottom, black 0%, black 10%, transparent 30%, transparent 100%, black 80%, black 100%);
         z-index: 0;
@@ -231,7 +231,7 @@
         position: absolute;
         bottom: 0;
         left: 0;
-        width: 90vw;
+        width: 100vw;
         height: 20%;
         background: linear-gradient(to top, black 0%, black 10%, transparent 30%, transparent 100%, rgba(0, 0, 0, 0.612) 80%, black 100%);
     }

3bb54c8f9aaeab3fd63d48cb9874c82f7df41b97
Dominique Karreman <s1169919@student.windesheim.nl>
2023-06-18 13:50:35 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>248Basketball</title>
+    <title>24/8 Basketball</title>
     <link rel="stylesheet" type="text/css" href="/styles/welcome.css">
 </head>
 
